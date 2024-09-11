9ea65e1d29d7618f810e9cdc8a9ec7c578ce96db
Patihan Prasongphol <patihan70@gmail.com>
2024-09-11 15:17:18 +0700

    init DS Queue

--- queue.js
+++ queue.js
@@ -1,3 +1,8 @@
 function Queue() {
-    
+    // TODO: print the queue
+    // TODO: enqueue the queue
+    // TODO: dequeue the queue
+    // TODO: print front of the queue
+    // TODO: print size of the queue
+    // TODO: isEmpty function
 }
