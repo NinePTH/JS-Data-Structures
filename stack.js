e76bc183f4146a71ba9725b16fbf0ce62428e35f
Patihan Prasongphol <patihan70@gmail.com>
2024-09-11 14:34:04 +0700

    init project with DS stack

---
+++ stack.js
@@ -0,0 +1,50 @@
+var stack = function() {
+    this.count = 0;
+    this.storage = {};
+
+    this.push = function(value) {
+        this.storage[this.count] = value;
+        this.count++;
+    }
+
+    this.pop = function() {
+        if (this.count === 0) {
+            return undefined;
+        }
+
+        this.count--;
+        let popResult = this.storage[this.count];
+        delete this.storage[this.count];
+        return popResult;
+    }
+
+    this.peek = function() {
+        return this.storage[this.count-1]
+    }
+
+    this.size = function() {
+        return this.count;
+    }
+}
+
+// * Example usage
+// This show how to use the stack to solve palindrome problem
+
+let letter = new stack();
+let word = "ok";
+let rword = "";
+for (let i = 0; i<word.length; i++){
+    letter.push(word[i]); 
+    // console.log(letter.storage);
+}
+
+for (let i = 0; i<word.length; i++){
+    let result = letter.pop();
+    rword += result;
+}
+
+if (rword === word) {
+    console.log(word + " is palindrome");
+} else {
+    console.log(word + " is not palindrome");
+}
