99c0761f1b21cc62c86e815e55d74f12af6bc4d2
Patihan Prasongphol <patihan70@gmail.com>
2024-09-11 16:15:02 +0700

    Complete: DS queue

--- queue.js
+++ queue.js
@@ -1,8 +1,38 @@
 function Queue() {
-    // TODO: print the queue
-    // TODO: enqueue the queue
-    // TODO: dequeue the queue
-    // TODO: print front of the queue
-    // TODO: print size of the queue
-    // TODO: isEmpty function
+    this.Collection = []; // Array of queue elements
+
+    this.print = function() {
+        return this.Collection;
+    }
+
+    this.enqueue = function(value) {
+        this.Collection.push(value);
+    }
+
+    this.dequeue = function() {
+        return this.Collection.shift();
+    }
+
+    this.front = function() {
+        return this.Collection[0];
+    }
+
+    this.size = function() {
+        return this.Collection.length;
+    }
+
+    this.isEmpty = function() {
+        return (this.Collection.length === 0);
+    }
 }
+
+let myQueue = new Queue();
+myQueue.enqueue("Nine");
+myQueue.enqueue("Luna");
+myQueue.enqueue("Bew");
+console.log(myQueue.print());
+console.log(myQueue.dequeue());
+console.log(myQueue.print());
+console.log(myQueue.dequeue());
+console.log(myQueue.print());
+console.log(myQueue.isEmpty());
